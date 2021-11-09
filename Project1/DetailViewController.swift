e24d4126191f30f5708a1c7cb74b261f3658f367
Marcos Rios <rioos.marcos@gmail.com>
2021-11-08 22:26:11 -0300

    Added new file sharing functionality.

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -15,6 +15,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -30,6 +31,17 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("no image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     /*
     // MARK: - Navigation
